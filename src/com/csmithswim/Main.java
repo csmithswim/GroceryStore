8b107cfe8c921cf90fd76254f235e8770f263248
Cullen S <cullensmi@gmail.com>
2020-10-23 17:35:03 -0400

    Cleaned up code

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -2,13 +2,6 @@ package com.csmithswim;
 
 public class Main {
     public static void main(String[] args) {
-        new Employee("joe");//
-        Employee employee1 = new Employee(
-                "john", "smith",
-                40, 15, 15);
 
-//        double payCheck = employee1.calculateGrossPaycheck();
-
-        System.out.println(employee1.toString());
     }
 }
