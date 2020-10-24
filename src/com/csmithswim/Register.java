225f38db938355718c1bc094601780100861cfe9
Cullen S <cullensmi@gmail.com>
2020-10-24 13:09:45 -0400

    Added more code

--- src/com/csmithswim/Register.java
+++ src/com/csmithswim/Register.java
@@ -7,4 +7,19 @@ public class Register extends GroceryStore {
     int registerNumber;
 
 
-}
+    public class Employee extends GroceryStore {
+
+        public String fName;
+        public String lName;
+        private Double hours;
+        private double extraHours;
+        private Double hourlyRate;
+
+        public Employee(String fName) {
+
+            this(fName,"smith",
+                    0, 0,
+                    11.75);
+        }
+
+    }
